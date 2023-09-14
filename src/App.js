ea54fc417e1abd63e4620f1d952ae064e8f65368
Subham Biswas <bsubham408@gmail.com>
2023-09-14 19:21:33 +0530

    Add some file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SeoWidget from './seoWidget'; // Use a relative path
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SeoWidget />
+      </header>
+    </div>
+  );
+}
+
+export default App;
